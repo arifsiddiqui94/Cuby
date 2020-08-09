831832ae1ded5078fcf4021d19ea596969110cc9
Mohammad Arif Siddiqui <mohammadarif.siddiqui@telusinternational.com>
2020-08-09 22:23:52 +0530

    landing page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,24 +6,36 @@ function header() {
     return (
         <div>
             <nav id="navbar" className="navbar fixed-top navbar-expand-lg navbar-light">
-                <div className="logo-container">
-                    <img src={logo} alt="logo" width="90" />
+                <div className="logo-container text-white">
+                    {/* <img src={logo} alt="logo" width="90" /> */}
+                    CUBY
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                 </div>
-                <div className="collapse navbar-collapse justify-content-around" id="navbarNav">
+                <div className="collapse navbar-collapse justify-content-end pr-5" id="navbarNav">
                     <ul className="navbar-nav" id="#navbar-nav">
                         <li className="nav-item">
                             <a className="nav-link" href="#" >HOME</a>
                         </li>
 
                         <li className="nav-item">
-                            <a className="nav-link" href="#team" >TEAM</a>
+                            <a className="nav-link" href="#team" ><span>VISION</span></a>
                         </li>
 
                         <li className="nav-item">
-                            <a className="nav-link" href="#contact" >CONTACT US</a>
+                            <a className="nav-link" href="#contact"><span>RESOURCES</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"><span>OUTREACH</span></a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#team"><span>TEAM</span></a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contact"><span>CONTACT</span></a>
                         </li>
                     </ul>
                 </div>
---
+++ src/components/home/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import AssembleVideo from '../assemble_video/index';
+import './style.css';
+
+function home() {
+    return (
+        <div className="container-fluid home">
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12 cuby">
+                    <h1>CUBY</h1>
+                    <p className="friend-text">Your friend in space</p>
+                    <p className="assemble-text mb-1 mt-5">Assemble. Code. Launch.</p>
+                    <button>Request Demo</button>
+                </div>
+
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <AssembleVideo />
+                </div>
+            </div>
+            <div className="row home-footer">
+                <span><i class="fab fa-github mr-4"></i></span>
+                <span><i class="fab fa-linkedin-in mr-4"></i></span>
+                <span><i class="fab fa-google"></i></span>
+            </div>
+        </div>
+    );
+}
+
+export default home;
